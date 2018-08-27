d74460201e5a93e179f0e7ba8c3d7eb2874727a2
Rowan de Haas <rowan.dehaas@stratisplatform.com>
2018-08-27 18:43:28 +1000

    Update RVM tests

--- src/Stratis.Bitcoin.Features.SmartContracts.Tests/ReflectionVirtualMachineTests.cs
+++ src/Stratis.Bitcoin.Features.SmartContracts.Tests/ReflectionVirtualMachineTests.cs
@@ -1,151 +1,105 @@
-﻿//using System;
-//using System.Text;
-//using NBitcoin;
-//using Stratis.Patricia;
-//using Stratis.SmartContracts;
-//using Stratis.SmartContracts.Core;
-//using Stratis.SmartContracts.Core.State;
-//using Stratis.SmartContracts.Executor.Reflection;
-//using Stratis.SmartContracts.Executor.Reflection.Compilation;
-//using Xunit;
+﻿using Moq;
+using NBitcoin;
+using Stratis.SmartContracts;
+using Stratis.SmartContracts.Core;
+using Stratis.SmartContracts.Core.State;
+using Stratis.SmartContracts.Executor.Reflection;
+using Stratis.SmartContracts.Executor.Reflection.Compilation;
+using Stratis.SmartContracts.Executor.Reflection.ContractLogging;
+using Xunit;
+using Block = Stratis.SmartContracts.Core.Block;
+using InternalHashHelper = Stratis.SmartContracts.Core.Hashing.InternalHashHelper;
 
-//namespace Stratis.Bitcoin.Features.SmartContracts.Tests
-//{
-//    public sealed class ReflectionVirtualMachineTests
-//    {
-//        private readonly Network network;
-//        private readonly ReflectionVirtualMachine vm;
+namespace Stratis.Bitcoin.Features.SmartContracts.Tests
+{
+    public sealed class ReflectionVirtualMachineTests
+    {
+        private readonly Network network;
+        private readonly ReflectionVirtualMachine vm;
 
-//        private static readonly Address TestAddress = (Address)"mipcBbFg9gMiCh81Kj8tqqdgoZub1ZJRfn";
+        private static readonly Address TestAddress = (Address)"mipcBbFg9gMiCh81Kj8tqqdgoZub1ZJRfn";
+        private ContractStateRepositoryRoot state;
+        private SmartContractState contractState;
 
-//        public ReflectionVirtualMachineTests()
-//        {
-//            // Take what's needed for these tests
-//            var context = new ContractExecutorTestContext();
-//            this.network = context.Network;
-//            this.vm = context.Vm;
-//        }
+        public ReflectionVirtualMachineTests()
+        {
+            // Take what's needed for these tests
+            var context = new ContractExecutorTestContext();
+            this.network = context.Network;
+            this.vm = context.Vm;
+            this.state = context.State;
+            this.contractState = new SmartContractState(
+                new Block(1, TestAddress),
+                new Message(TestAddress, TestAddress, 0),
+                new PersistentState(new PersistenceStrategy(this.state),
+                    context.ContractPrimitiveSerializer, TestAddress.ToUint160(this.network)),
+                new GasMeter((Gas)5000000),
+                new ContractLogHolder(this.network),
+                Mock.Of<IInternalTransactionExecutor>(),
+                new InternalHashHelper(),
+                () => 1000);
+        }
 
-//        [Fact]
-//        public void VM_ExecuteContract_WithoutParameters()
-//        {
-//            //Get the contract execution code------------------------
-//            SmartContractCompilationResult compilationResult = SmartContractCompiler.CompileFile("SmartContracts/StorageTest.cs");
-//            Assert.True(compilationResult.Success);
+        [Fact]
+        public void VM_ExecuteContract_WithoutParameters()
+        {
+            SmartContractCompilationResult compilationResult = SmartContractCompiler.CompileFile("SmartContracts/StorageTest.cs");
+            Assert.True(compilationResult.Success);
 
-//            byte[] contractExecutionCode = compilationResult.Compilation;
-//            //-------------------------------------------------------
+            byte[] contractExecutionCode = compilationResult.Compilation;
 
-//            //Set the calldata for the transaction----------
-//            var callData = new CallData((Gas)5000000, new uint160(1), "StoreData");
+            var callData = new CallData((Gas)5000000, new uint160(1), "NoParamsTest");
 
-//            Money value = Money.Zero;
-//            //-------------------------------------------------------
+            this.state.SetCode(callData.ContractAddress, contractExecutionCode);
+            this.state.SetContractType(callData.ContractAddress, "StorageTest");
 
-//            var repository = new ContractStateRepositoryRoot(new NoDeleteSource<byte[], byte[]>(new MemoryDictionarySource()));
-//            IContractStateRepository stateRepository = repository.StartTracking();
+            VmExecutionResult result = this.vm.ExecuteMethod(
+                this.state,
+                callData,
+                this.contractState);
 
-//            var gasMeter = new GasMeter(callData.GasLimit);
+            Assert.Null(result.ExecutionException);
+            Assert.True((bool)result.Result);
+        }
 
-//            uint160 address = TestAddress.ToUint160(this.network);
+        [Fact]
+        public void VM_ExecuteContract_WithParameters()
+        {
+            SmartContractCompilationResult compilationResult = SmartContractCompiler.CompileFile("SmartContracts/StorageTest.cs");
+            Assert.True(compilationResult.Success);
 
-//            var transactionContext = new TransactionContext(uint256.One, 1, address, address, 0);
+            byte[] contractExecutionCode = compilationResult.Compilation;
+            var methodParameters = new object[] { (int)5 };
+            var callData = new CallData((Gas)5000000, new uint160(1), "OneParamTest", methodParameters);
+            
+            this.state.SetCode(callData.ContractAddress, contractExecutionCode);
+            this.state.SetContractType(callData.ContractAddress, "StorageTest");
 
-//            repository.SetCode(callData.ContractAddress, contractExecutionCode);
-//            repository.SetContractType(callData.ContractAddress, "StorageTest");
+            VmExecutionResult result = this.vm.ExecuteMethod(
+                this.state,
+                callData,
+                this.contractState);
 
-//            VmExecutionResult result = this.vm.ExecuteMethod(gasMeter,
-//                repository,
-//                callData,
-//                transactionContext);
+            Assert.Null(result.ExecutionException);
+            Assert.Equal(methodParameters[0], result.Result);
+        }
 
-//            stateRepository.Commit();
+        [Fact]
+        public void VM_CreateContract_WithParameters()
+        {
+            SmartContractCompilationResult compilationResult = SmartContractCompiler.CompileFile("SmartContracts/Auction.cs");
+            Assert.True(compilationResult.Success);
 
-//            Assert.Equal(Encoding.UTF8.GetBytes("TestValue"), stateRepository.GetStorageValue(callData.ContractAddress, Encoding.UTF8.GetBytes("TestKey")));
-//            Assert.Equal(Encoding.UTF8.GetBytes("TestValue"), repository.GetStorageValue(callData.ContractAddress, Encoding.UTF8.GetBytes("TestKey")));
-//        }
+            byte[] contractExecutionCode = compilationResult.Compilation;
+            var methodParameters = new object[] { (ulong)5 };
+            var callData = new CreateData((Gas)5000000, contractExecutionCode, methodParameters);
 
-//        [Fact]
-//        public void VM_ExecuteContract_WithParameters()
-//        {
-//            //Get the contract execution code------------------------
-//            SmartContractCompilationResult compilationResult = SmartContractCompiler.CompileFile("SmartContracts/StorageTestWithParameters.cs");
-//            Assert.True(compilationResult.Success);
+            VmExecutionResult result = this.vm.Create(
+                this.state,
+                callData,
+                this.contractState);
 
-//            byte[] contractExecutionCode = compilationResult.Compilation;
-//            //-------------------------------------------------------
-
-//            //Set the calldata for the transaction----------
-//            var methodParameters = new object[] { (short)5 };
-//            var callData = new CallData((Gas)5000000, new uint160(1), "StoreData", methodParameters);
-//            Money value = Money.Zero;
-//            //-------------------------------------------------------
-
-//            var repository = new ContractStateRepositoryRoot(new NoDeleteSource<byte[], byte[]>(new MemoryDictionarySource()));
-//            IContractStateRepository track = repository.StartTracking();
-
-//            var gasMeter = new GasMeter(callData.GasLimit);
-
-//            uint160 address = TestAddress.ToUint160(this.network);
-
-//            var transactionContext = new TransactionContext(uint256.One, 1, address, address, value);
-
-//            repository.SetCode(callData.ContractAddress, contractExecutionCode);
-//            repository.SetContractType(callData.ContractAddress, "StorageTestWithParameters");
-
-//            VmExecutionResult result = this.vm.ExecuteMethod(gasMeter,
-//                repository,
-//                callData,
-//                transactionContext);
-
-//            track.Commit();
-
-//            Assert.Equal(5, BitConverter.ToInt16(track.GetStorageValue(callData.ContractAddress, Encoding.UTF8.GetBytes("orders")), 0));
-//            Assert.Equal(5, BitConverter.ToInt16(repository.GetStorageValue(callData.ContractAddress, Encoding.UTF8.GetBytes("orders")), 0));
-//        }
-
-//        [Fact]
-//        public void VM_CreateContract_WithParameters()
-//        {
-//            //Get the contract execution code------------------------
-//            SmartContractCompilationResult compilationResult =
-//                SmartContractCompiler.CompileFile("SmartContracts/Auction.cs");
-
-//            Assert.True(compilationResult.Success);
-//            byte[] contractExecutionCode = compilationResult.Compilation;
-//            //-------------------------------------------------------            
-
-//            //Set the calldata for the transaction----------
-//            var methodParameters = new object[] { (ulong)5 };
-//            var callData = new CreateData((Gas)5000000, contractExecutionCode, methodParameters);
-
-//            Money value = Money.Zero;
-//            //-------------------------------------------------------            
-
-//            var repository = new ContractStateRepositoryRoot(new NoDeleteSource<byte[], byte[]>(new MemoryDictionarySource()));
-//            IContractStateRepository track = repository.StartTracking();
-
-//            var gasMeter = new GasMeter(callData.GasLimit);
-
-//            var transactionContext = new TransactionContext(
-//                txHash: uint256.One,
-//                blockHeight: 1,
-//                coinbase: TestAddress.ToUint160(this.network),
-//                sender: TestAddress.ToUint160(this.network),
-//                amount: value
-//                );
-
-//            VmExecutionResult result = this.vm.Create(gasMeter,
-//                repository,
-//                callData,
-//                transactionContext, TODO);
-
-//            track.Commit();
-
-//            var endBlockValue = track.GetStorageValue(result.NewContractAddress,
-//                Encoding.UTF8.GetBytes("EndBlock"));
-//            Assert.Equal(6, BitConverter.ToInt16(endBlockValue, 0));
-//            Assert.Equal(TestAddress.ToUint160(this.network).ToBytes(), track.GetStorageValue(result.NewContractAddress, Encoding.UTF8.GetBytes("Owner")));
-//        }
-//    }
-//}
+            Assert.Null(result.ExecutionException);
+        }
+    }
+}
