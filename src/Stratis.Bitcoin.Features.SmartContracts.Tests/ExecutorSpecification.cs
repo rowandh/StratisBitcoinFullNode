f950e97a3d4d6f4d7c58a91711b985e42c089fbf
Rowan de Haas <rowan.dehaas@stratisplatform.com>
2018-09-05 13:48:12 +1000

    Add Executor specification for contract call

--- src/Stratis.Bitcoin.Features.SmartContracts.Tests/ExecutorSpecification.cs
+++ src/Stratis.Bitcoin.Features.SmartContracts.Tests/ExecutorSpecification.cs
@@ -21,12 +21,12 @@ namespace Stratis.Bitcoin.Features.SmartContracts.Tests
             var network = new SmartContractsRegTest();
             uint160 newContractAddress = uint160.One;
             var gasConsumed = (Gas) 100;
-            var code = new byte[] {0xAA, 0xBB, 0xCC};
-            var contractTxData = new ContractTxData(1, 1, (Gas) 1000, code);
+            var rawTransactionData = new byte[] {0xAA, 0xBB, 0xCC};
+            var contractTxData = new ContractTxData(1, 1, (Gas) 1000, rawTransactionData);
             var refund = new Money(0);
             const ulong mempoolFee = 2UL; // MOQ doesn't like it when you use a type with implicit conversions (Money)
             ISmartContractTransactionContext context = Mock.Of<ISmartContractTransactionContext>(c => 
-                c.Data == code &&
+                c.Data == rawTransactionData &&
                 c.MempoolFee == mempoolFee &&
                 c.Sender == uint160.One &&
                 c.CoinbaseAddress == uint160.Zero);
@@ -86,7 +86,7 @@ namespace Stratis.Bitcoin.Features.SmartContracts.Tests
 
             sut.Execute(context);
 
-            callDataSerializer.Verify(s => s.Deserialize(code), Times.Once);
+            callDataSerializer.Verify(s => s.Deserialize(rawTransactionData), Times.Once);
             
             stateFactory.Verify(sf => sf
                 .Create(
@@ -107,6 +107,111 @@ namespace Stratis.Bitcoin.Features.SmartContracts.Tests
                     context,
                     internalTransfers,
                     false), 
+                Times.Once);
+
+            refundProcessor.Verify(t => t
+                    .Process(
+                        contractTxData,
+                        mempoolFee,
+                        context.Sender,
+                        It.IsAny<Gas>(),
+                        vmExecutionResult.ExecutionException),
+                Times.Once);
+        }
+
+        [Fact]
+        public void Call_Contract_Success()
+        {
+            var network = new SmartContractsRegTest();
+            uint160 contractAddress = uint160.One;
+            var gasConsumed = (Gas)100;
+            var rawTransactionData = new byte[] { 0xAA, 0xBB, 0xCC };
+            var parameters = new object[] {1, "a"};
+            var contractTxData = new ContractTxData(1, 1, (Gas)1000, contractAddress, "Test", "", parameters);
+            var refund = new Money(0);
+            const ulong mempoolFee = 2UL; // MOQ doesn't like it when you use a type with implicit conversions (Money)
+            ISmartContractTransactionContext context = Mock.Of<ISmartContractTransactionContext>(c =>
+                c.Data == rawTransactionData &&
+                c.MempoolFee == mempoolFee &&
+                c.Sender == uint160.One &&
+                c.CoinbaseAddress == uint160.Zero);
+
+            var logger = new Mock<ILogger>();
+            ILoggerFactory loggerFactory = Mock.Of<ILoggerFactory>
+                    (l => l.CreateLogger(It.IsAny<string>()) == logger.Object);
+
+            var callDataSerializer = new Mock<ICallDataSerializer>();
+            callDataSerializer
+                .Setup(s => s.Deserialize(It.IsAny<byte[]>()))
+                .Returns(Result.Ok(contractTxData));
+
+            var vmExecutionResult = VmExecutionResult.Success(null, null);
+
+            var contractStateRoot = new Mock<IContractStateRoot>();
+            var transferProcessor = new Mock<ISmartContractResultTransferProcessor>();
+
+            (Money refund, List<TxOut>) refundResult = (refund, new List<TxOut>());
+            var refundProcessor = new Mock<ISmartContractResultRefundProcessor>();
+
+            refundProcessor
+                .Setup(r => r.Process(
+                    contractTxData,
+                    mempoolFee,
+                    context.Sender,
+                    It.IsAny<Gas>(),
+                    vmExecutionResult.ExecutionException))
+                .Returns(refundResult);
+
+            var stateTransitionResult =
+                new StateTransitionResult(gasConsumed, contractAddress, true, vmExecutionResult);
+
+            var internalTransfers = new List<TransferInfo>().AsReadOnly();
+            var stateMock = new Mock<IState>();
+            stateMock.Setup(s => s.Apply(It.IsAny<ExternalCallMessage>()))
+                .Returns(stateTransitionResult);
+            stateMock.SetupGet(p => p.InternalTransfers).Returns(internalTransfers);
+
+            var stateFactory = new Mock<IStateFactory>();
+            stateFactory.Setup(sf => sf.Create(
+                contractStateRoot.Object,
+                It.IsAny<IBlock>(),
+                context.TxOutValue,
+                context.TransactionHash,
+                contractTxData.GasLimit))
+            .Returns(stateMock.Object);
+
+            var sut = new Executor(
+                loggerFactory,
+                callDataSerializer.Object,
+                contractStateRoot.Object,
+                refundProcessor.Object,
+                transferProcessor.Object,
+                network,
+                stateFactory.Object);
+
+            sut.Execute(context);
+
+            callDataSerializer.Verify(s => s.Deserialize(rawTransactionData), Times.Once);
+
+            stateFactory.Verify(sf => sf
+                .Create(
+                    contractStateRoot.Object,
+                    It.IsAny<IBlock>(),
+                    context.TxOutValue,
+                    context.TransactionHash,
+                    contractTxData.GasLimit),
+                Times.Once);
+
+            stateMock.Verify(sm => sm
+                .Apply(It.IsAny<ExternalCallMessage>()), Times.Once);
+
+            transferProcessor.Verify(t => t
+                .Process(
+                    contractStateRoot.Object,
+                    contractAddress,
+                    context,
+                    internalTransfers,
+                    false),
                 Times.Once);
 
             refundProcessor.Verify(t => t
