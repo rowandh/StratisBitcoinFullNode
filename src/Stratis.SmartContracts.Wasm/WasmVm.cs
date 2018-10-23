0e5d46c85a2eea7d42edc25966fa4089127388cb
Rowan de Haas <rowan.dehaas@stratisplatform.com>
2018-10-24 10:03:12 +1100

    Comments

--- src/Stratis.SmartContracts.Wasm/WasmVm.cs
+++ src/Stratis.SmartContracts.Wasm/WasmVm.cs
@@ -6,11 +6,19 @@ namespace Stratis.SmartContracts.Wasm
     {
         public VmResult Create(ulong gas, ulong amount, byte[] data, byte[] sender)
         {
+            // Invoke
+            // Get WASM bytecode left in memory
+            // Validate
+            // Inject gas metering
+            // Store
             throw new NotImplementedException();
         }
 
         public VmResult Call(ulong gas, ulong amount, byte[] data, byte[] sender, byte[] destination)
         {
+            // Retrieve code
+            // Load WASM module
+            // Invoke `main` with supplied data
             throw new NotImplementedException();
         }
     }
