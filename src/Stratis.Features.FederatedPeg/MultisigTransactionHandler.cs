663eb2afa6d11798d3ec5e29607ebd97306aba4a
Rowan de Haas <rowandh@users.noreply.github.com>
2019-06-14 14:22:41 +1000

    Remove unused/unnecessary code

--- src/Stratis.Features.FederatedPeg/MultisigTransactionHandler.cs
+++ src/Stratis.Features.FederatedPeg/MultisigTransactionHandler.cs
@@ -11,96 +11,44 @@ using Stratis.Features.FederatedPeg.Interfaces;
 using Stratis.Features.FederatedPeg.Models;
 using Stratis.Features.FederatedPeg.TargetChain;
 using Stratis.Features.FederatedPeg.Wallet;
-using TransactionBuildContext = Stratis.Bitcoin.Features.Wallet.TransactionBuildContext;
 
 namespace Stratis.Features.FederatedPeg
 {
     public interface IMultisigTransactionHandler
     {
         /// <summary>
-        /// Builds a new multisig transaction based on information from the <see cref="TransactionBuildContext"/>.
+        /// Builds a new multisig transaction based on information from the <see cref="FederatedPeg.Wallet.TransactionBuildContext"/>.
         /// </summary>
-        /// <param name="context">The context that is used to build a new transaction.</param>
-        /// <param name="secrets">List of mnemonic-passphrase pairs</param>
         /// <returns>The new transaction.</returns>
-        Transaction BuildTransaction(TransactionBuildContext context, SecretModel[] secrets);
-
         Transaction BuildTransaction(BuildMultisigTransactionRequest request);
     }
 
     /// <summary>
     /// A handler that has various functionalities related to transaction operations.
     /// </summary>
-    public class MultisigTransactionHandler : WalletTransactionHandler, IMultisigTransactionHandler
+    public class MultisigTransactionHandler : IMultisigTransactionHandler
     {
         private readonly IFederationWalletTransactionHandler federationWalletTransactionHandler;
 
         private readonly IFederationWalletManager federationWalletManager;
+
         private readonly IFederatedPegSettings federatedPegSettings;
 
         private readonly ILogger logger;
 
         private readonly Network network;
 
-        private readonly StandardTransactionPolicy transactionPolicy;
-
-        private readonly IWalletManager walletManager;
-
-        private readonly IWalletFeePolicy walletFeePolicy;
-
-        public MultisigTransactionHandler(
-            ILoggerFactory loggerFactory,
-            IWalletManager walletManager,
-            IWalletFeePolicy walletFeePolicy,
+        public MultisigTransactionHandler(ILoggerFactory loggerFactory,
             Network network,
-            StandardTransactionPolicy transactionPolicy,
             IFederationWalletTransactionHandler federationWalletTransactionHandler,
             IFederationWalletManager federationWalletManager,
             IFederatedPegSettings federatedPegSettings)
-            : base(loggerFactory, walletManager, walletFeePolicy, network, transactionPolicy)
         {
+            this.logger = loggerFactory.CreateLogger(this.GetType().FullName);
+            this.network = network;
             this.federationWalletTransactionHandler = federationWalletTransactionHandler;
             this.federationWalletManager = federationWalletManager;
             this.federatedPegSettings = federatedPegSettings;
-            this.logger = loggerFactory.CreateLogger(this.GetType().FullName);
-        }
-
-        /// <inheritdoc />
-        public Transaction BuildTransaction(TransactionBuildContext context, SecretModel[] secrets)
-        {
-            if (secrets == null || secrets.Length == 0)
-                throw new WalletException("Could not build the transaction. Details: no private keys provided");
-
-            this.InitializeTransactionBuilder(context);
-
-            //if (context.Shuffle)
-            //    context.TransactionBuilder.Shuffle();
-
-            Transaction unsignedTransaction = context.TransactionBuilder.BuildTransaction(false);
-
-            var signedTransactions = new List<Transaction>();
-            foreach (SecretModel secret in secrets)
-            {
-                TransactionBuildContext contextCopy = context.Clone(this.network);
-
-                this.InitializeTransactionBuilder(contextCopy);
-
-                var mnemonic = new Mnemonic(secret.Mnemonic);
-                ExtKey extKey = mnemonic.DeriveExtKey(secret.Passphrase);
-                Transaction transaction = contextCopy.TransactionBuilder.AddKeys(extKey.PrivateKey).SignTransaction(unsignedTransaction);
-
-                signedTransactions.Add(transaction);
-            }
-
-            context.TransactionBuilder.SignTransaction(unsignedTransaction);
-            Transaction combinedTransaction = context.TransactionBuilder.CombineSignatures(signedTransactions.ToArray());
-
-            if (context.TransactionBuilder.Verify(combinedTransaction, out TransactionPolicyError[] errors))
-                return combinedTransaction;
-
-            string errorsMessage = string.Join(" - ", errors.Select(s => s.ToString()));
-            LoggerExtensions.LogError(this.logger, $"Build transaction failed: {errorsMessage}");
-            throw new WalletException($"Could not build the transaction. Details: {errorsMessage}");
         }
 
         public Transaction BuildTransaction(BuildMultisigTransactionRequest request)
@@ -181,32 +129,6 @@ namespace Stratis.Features.FederatedPeg
             this.logger.LogTrace("(-)[FAIL]");
 
             return null;
-        }
-
-        /// <summary>
-        /// Initializes the context transaction builder from information in <see cref="TransactionBuildContext"/>.
-        /// </summary>
-        /// <param name="context">Transaction build context.</param>
-        protected override void InitializeTransactionBuilder(TransactionBuildContext context)
-        {
-            Guard.NotNull(context, nameof(context));
-            Guard.NotNull(context.Recipients, nameof(context.Recipients));
-            Guard.NotNull(context.AccountReference, nameof(context.AccountReference));
-
-            // If inputs are selected by the user, we just choose them all.
-            if (context.SelectedInputs != null && context.SelectedInputs.Any())
-            {
-                context.TransactionBuilder.CoinSelector = new AllCoinsSelector();
-            }
-
-            this.AddRecipients(context);
-            this.AddOpReturnOutput(context);
-            this.AddCoins(context);
-            this.FindChangeAddress(context);
-            this.AddFee(context);
-
-            if (context.Time.HasValue)
-                context.TransactionBuilder.SetTimeStamp(context.Time.Value);
         }
     }
 }
